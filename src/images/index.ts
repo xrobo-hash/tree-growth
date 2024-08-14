49961aa151f077746a325f6afd5a323e3a1b9850
xrobo-hash <xrobo-collection@gmail.com>
2024-08-14 07:55:18 +0700

    first commit

---
+++ src/images/index.ts
@@ -0,0 +1,19 @@
+import treeExchange from "./tree-exchange.png";
+import binanceLogo from "./binance-logo.png";
+import dollarCoin from "./dollar-coin.png";
+import dailyReward from "./daily-reward.png";
+import dailyCipher from "./daily-cipher.png";
+import dailyCombo from "./daily-combo.png";
+import mainCharacter from "./main-character.png";
+import treeCoin from "./tree-coin.png";
+
+export {
+    treeExchange,
+    binanceLogo,
+    dollarCoin,
+    dailyReward,
+    dailyCipher,
+    dailyCombo,
+    mainCharacter,
+    treeCoin
+};
